13e9556ae694c369d165d49d46650a9c24013044
Adam Tumielewicz <atumielewicz@creatuity.pl>
2015-07-24 09:27:59 +0200

    auto create deb file - fucntion added

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ PACKAGE = $(shell basename `pwd`)
 make:
 	dpkg-deb -z9 -Zgzip --build dist $(PACKAGE).deb
 
+install:	
+	scp -P 8888 $(PACKAGE).deb dev:/storage/creatuity-deb-repository/amd64/
+	ssh  devpl "cd /storage/creatuity-deb-repository && dpkg-scanpackages amd64 | gzip -9c > /storage/creatuity-deb-repository/amd64/Packages.gz"
+
+clean:
+	rm $(PACKAGE).deb
--- basemod-init.sh
+++ basemod-init.sh
@@ -1,62 +1,123 @@
 #!/bin/bash
 
-DIR_TREE="creatuity-basemod/dist/usr/bin"
+######################################################################
+#Description: This script install, update and populate creatuity-basemod module
+#Author: Adam Tumielewicz <atumielewicz@cratuity.pl>
+#Copyright 2015
+#License: Creatuity Corp.
+#http://creativecommons.org/licenses/by-sa/4.0/legalcode
+######################################################################
+
+
+PACKAGE_DIR_TREE="dist/usr/bin"
+DEBIAN_DIR="dist/DEBIAN"
 BASEMODREPOLINK="https://github.com/Adam22/DrupalBasicConfiguration.git"
 CURRENTPATH=`pwd`
 ACTION=$1
+VERSION=`cat ./VERSION`
+SCRIPT=`basename ${BASH_SOURCE[0]}`
 
 function cloneBasemod {
-	if ! [ -d "basmode" ] && [$(basename `pwd`) -eq "basemod"]; then		
-		mkdir basemod	
-		cd basemod
+	if ! [ -d "creatuity-basemod" ] && [$(basename `pwd`) -eq "creatuity-basemod"]; then		
+		mkdir creatuity-basemod	
+		cd creatuity-basemod
 	fi	
 	if [ -d ".git" ]; then
 		echo .git exist;
 	else
   		git rev-parse --git-dir 2> /dev/null;
-  		git clone $BASEMODREPOLINK basemod/
+  		git clone $BASEMODREPOLINK ./
   		git clean -df
 	fi;
 }
+
 function updateBasemod {
 	if [ -d ".git" ]; then
 		echo .git exist;
 		git pull origin master 
 	else
-		echo "no git repository"
+		echo "No git repository"
+		exit 1
 	fi
 }
+
+function parseControlFile {
+	sed -i 's/Version: [0-9]\.[0-9]/'"$VERSION"'/' ./control
+}
+
 function preparePackage {
-	if ! [ -d "creatuity-basemod" ]; then
-		mkdir -p $DIR_TREE
-	fi
-	cd creatuity-basemod
-	if [[ -d "dist" ]]; then
+	if  [ -d "dist" ]; then
+		mkdir -p $DEBIAN_DIR
+		mkdir -p $PACKAGE_DIR_TREE
+		parseControlFile;
+		cp basemod* dist/usr/bin/
+		cp control dist/DEBIAN/
 		if [[ -f "Makefile" ]]; then
 			make -f Makefile
+			make install Makefile
 		fi
 	fi
 }
 
 function createBasemodDeb {
 	cloneBasemod;
-	updateBasemod
+	updateBasemod;	
+	preparePackage;
 }
+
+function populate {
+	ssh -p 8888 dev
+	ssh atumielewicz
+	apt-get install creatuity-basemod
+}
+
+###########################
+# getopts section
+
+OPT_A=A
+OPT_B=B
+OPT_C=C
+OPT_D=D
+
+#Set fonts for Help.
+#NORM=`tput sgr0`
+#BOLD=`tput bold`
+#REV=`tput smso`
+
+function help {
+	echo -e \\n "Help documentation for ${BOLD}${SCRIPT}. ${NORM}"\\n
+	echo -e "${REV}Basic usage:${NORM} ${BOLD}$SCRIPT file.ext${NORM}"\\n
+	echo "Command line switches are optional. The following switches are recognized."
+	echo "${REV}-a${NORM}  --Sets the value for option ${BOLD}a${NORM}. Default is ${BOLD}A${NORM}."
+	echo "${REV}-b${NORM}  --Sets the value for option ${BOLD}b${NORM}. Default is ${BOLD}B${NORM}."
+	echo "${REV}-c${NORM}  --Sets the value for option ${BOLD}c${NORM}. Default is ${BOLD}C${NORM}."
+	echo "${REV}-d${NORM}  --Sets the value for option ${BOLD}d${NORM}. Default is ${BOLD}D${NORM}."
+	echo -e "${REV}-h${NORM}  --Displays this help message. No further functions are performed."\\n
+	echo -e "Example: ${BOLD}$SCRIPT -a foo -b man -c chu -d bar file.ext${NORM}"\\n
+	exit 1
+}
+
+
+
 while (( "$#" )); do
 	case $ACTION in
-		'--install-basemod')
-			./basemod-test.sh
+		'install')
+			./basemod.sh install
 			;;
-		'--update-repository')
+		'sym')
+			./basemod.sh sym
 			;;
-		'--updade-basemod')
-			preparePackage;
-			pwd
-			cat VERSION
+		'populate-basemod')
+			populate-basemod;
+			;;
+		'update-basemod')
+			createBasemodDeb;
 			;;
 			*)
-			echo $1
+			help
 			;;
 	esac
 shift
 done
+
+
